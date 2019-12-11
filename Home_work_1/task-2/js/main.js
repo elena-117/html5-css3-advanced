20b992adb0d1f47224931025c6c3bb4926cf218a
elena-117 <3effir@gmail.com>
2019-12-11 10:31:36 +0300

    Дом. задание 1 (микроданные, геолокация)

---
+++ Home_work_1/task-2/js/main.js
@@ -0,0 +1,93 @@
+window.addEventListener('load', init, false);
+
+const popap = document.getElementById('popap');
+let posMap;
+let infoMap;
+
+function init() {
+
+   openPopap();
+   closePopap();
+
+}
+
+function openPopap() {
+   let linksElem = document.getElementsByClassName('hotel-card-map');
+
+   for (let i = 0; i < linksElem.length; i++) {
+      linksElem[i].addEventListener('click', function () {
+         switch (linksElem[i].dataset.name) {
+            case 'hotel-1':
+               posMap = {
+                  lat: -42.575112,
+                  lng: 171.479091,
+               };
+               info = new google.maps.InfoWindow({
+                  content: '<h3>Scenic Hotel Auckland</h3>'
+               });
+               initMap();
+               break;
+
+            case 'hotel-2':
+               posMap = {
+                  lat: -41.280003,
+                  lng: 174.780502,
+               };
+               info = new google.maps.InfoWindow({
+                  content: '<h3>Mercure Hotel</h3>'
+               });
+               initMap();
+               break;
+
+            case 'hotel-3':
+               posMap = {
+                  lat: -38.845718,
+                  lng: 174.767440,
+               };
+               info = new google.maps.InfoWindow({
+                  content: '<h3>Best Western Ellersli...</h3>'
+               });
+               initMap();
+               break;
+
+            case 'hotel-4':
+               posMap = {
+                  lat: -36.841971,
+                  lng: 174.750537,
+               };
+               info = new google.maps.InfoWindow({
+                  content: '<h3>Hotel Grand Windsor</h3>'
+               });
+               initMap();
+               break;
+         }
+         popap.style.display = 'block';
+      })
+   }
+}
+
+function initMap() {
+   let opt = {
+      center: posMap,
+      zoom: 10,
+   };
+
+   let map = new google.maps.Map(document.getElementById('map'), opt);
+
+   let marker = new google.maps.Marker({
+      position: posMap,
+      map: map,
+   });
+
+   marker.addListener('click', function () {
+      info.open(map, marker);
+   });
+}
+
+function closePopap() {
+   document.addEventListener('mouseup', function (e) {
+      if (e.target === popap) {
+         popap.style.display = 'none';
+      }
+   })
+}
